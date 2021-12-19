a47497c444ec2478fb4c2fe012183af757c76f93
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2021-12-19 20:01:11 +0900

    while文

--- JS2章/app.js
+++ JS2章/app.js
@@ -1,9 +1,10 @@
-let orange = 130;
-let apple = 120;
-if (orange < apple) {
-  alert('みかんの値段がりんごより安い');
-} else if (orange == apple) {
-  alert('みかんとりんごが同じ値段');
-} else {
-  alert('みかんの値段がりんごより高い');
+let max = 100;
+let num = 1;
+let count = 0;
+
+while (num < max) {
+  num = num * 2;
+  count = count + 1;
 }
+
+alert('2をかけ続けて' + max + 'を越えるのに必要だった回数は' + count + '回です。');
