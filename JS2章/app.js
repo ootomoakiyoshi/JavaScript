1b81da701550871c01e1ea0ab815278d74636052
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2021-12-19 19:55:49 +0900

    if分

--- JS2章/app.js
+++ JS2章/app.js
@@ -1,2 +1,9 @@
-let int1 = 1;
-alert(int1);
+let orange = 130;
+let apple = 120;
+if (orange < apple) {
+  alert('みかんの値段がりんごより安い');
+} else if (orange == apple) {
+  alert('みかんとりんごが同じ値段');
+} else {
+  alert('みかんの値段がりんごより高い');
+}
