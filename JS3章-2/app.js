775490be1ccf9a9797d34e152dab42ee2bcbc266
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2021-12-20 19:45:57 +0900

    [Finish]JS3章

--- JS3章-2/app.js
+++ JS3章-2/app.js
@@ -1,33 +1,32 @@
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
-
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)) {
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
 let js_hand = getJShand();
-
-// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
 let judge = winLose(user_hand, js_hand);
 
-// 結果を表示する
-alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
-
-// ランダムでじゃんけんの手を作成する関数
-function getJShand() {
-  let js_hand_num = Math.floor(Math.random() * 3);
-  let hand_name;
-
-  if (js_hand_num == 0) {
-    hand_name = "グー";
-  } else if (js_hand_num == 1) {
-    hand_name = "チョキ";
-  } else if (js_hand_num == 2) {
-    hand_name = "パー";
-  }
-
-  return hand_name;
+if (user_hand != null) {
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+  alert("またチャレンジしてね")
 }
 
-// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+function getJShand() {
+  let js_hand_num = Math.floor(Math.random() * 3);
+  let hand;
+  if (js_hand_num == 0) {
+    hand = "グー";
+  } else if (js_hand_num == 1) {
+    hand = "チョキ";
+  } else if (js_hand_num == 2) {
+    hand = "パー";
+  }
+  return hand;
+}
+
 function winLose(user, js) {
   let winLoseStr;
-
   if (user == "グー") {
     if (js == "グー") {
       winLoseStr = "あいこ";
@@ -53,6 +52,5 @@ function winLose(user, js) {
       winLoseStr = "あいこ";
     }
   }
-
   return winLoseStr;
 }
