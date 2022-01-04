11729435c7e2f58526f2bc1176b71a447b18836e
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2022-01-04 18:27:20 +0900

    自動スライド

--- SlideShow-Lesson/js/script.js
+++ SlideShow-Lesson/js/script.js
@@ -13,7 +13,7 @@ $(".theTarget").skippr({
   // ナビゲーション矢印の表示（trueで表示）
   arrows: true,
   // スライドショーの自動再生（falseで自動再生なし）
-  autoPlay: false,
+  autoPlay: true,
   // 自動再生時のスライド切替間隔（ミリ秒）
   autoPlayDuration: 5000,
   // キーボードの矢印キーによるスライド送りの設定（trueで有効）
